18a6bb8791971e0fefb109a79be3dc7dd376da26
RESHMADUTTA <104962226+RESHMADUTTA@users.noreply.github.com>
2024-06-09 15:30:06 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>TripSync- Simplifying Travel Planning and Management</title>
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   <script>
+      $(document).ready(function(){
+          $(".scroll-top").click(function() {
+              $("html, body").animate({ 
+                  scrollTop: 0 
+              }, "slow");
+              return false;
+          });
+      });
+   </script>
+</head>
+<body>
+<!-- header section starts  -->
+<section class="header">
+   <a href="home.php" class="logo"><img src="images/logo.jpg" width="85" height="75"></a>
+   <nav class="navbar">
+      <a href="home.php" class="active">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+   <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+<!-- header section ends -->
+<!-- home section starts  -->
+<section class="home">
+   <div class="swiper home-slider">
+      <div class="swiper-wrapper">
+         <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+               <span>tour arround the universe</span>
+               <h3>Discover the world, discover yourself</h3>
+               <a href="package.php" class="btn">See more</a>
+            </div>
+         </div>
+         <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+            <div class="content">
+               <span>express the new destination</span>
+               <h3>Wander often, wonder always</h3>
+               <a href="package.php" class="btn">See more</a>
+            </div>
+         </div>
+         <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+               <span>make your tour effective</span>
+               <h3>Journey with us, explore your dreams</h3>
+               <a href="package.php" class="btn">See more</a>
+            </div>
+         </div>
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+   </div>
+</section>
+<!-- home section ends -->
+<!-- services section starts  -->
+<section class="services">
+   <h1 class="heading-title"> our services </h1>
+   <div class="box-container">
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+   </div>
+</section>
+<!-- services section ends -->
+<!-- home about section starts  -->
+<section class="home-about">
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+   <div class="content">
+      <h3>about us</h3>
+      <p>Welcome to TripSync! We're a passionate travel agency dedicated to crafting unforgettable journeys and experiences. With our expert team and personalized itineraries, we turn your travel dreams into reality. Explore the world with confidence, knowing every detail is handled with care. Let's create your next adventure together!</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+</section>
+<!-- home about section ends -->
+<!-- home packages section starts  -->
+<section class="home-packages">
+   <h1 class="heading-title"> our packages </h1>
+   <div class="box-container">
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Dubai Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Dubai top selling packages!</p>
+            <h2>Rs. 29,990</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Delhi Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Delhi top selling packages!</p>
+            <h2>Rs. 12,100</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Japan Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Japan top selling packages!</p>
+            <h2>Rs. 98,990</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+   </div>
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+</section>
+<!-- home packages section ends -->
+
+
+
+
+<!-- home offer section starts  -->
+<section class="home-offer home-packages">
+   <div class="content">
+      <div class="offerimage">
+      <img src="images/offer.jpg">
+      </div>
+   </div>
+   <div class="content">
+      <h3>upto 30% off</h3>
+      <p>The Antalya Paradise Resort is centrally located within walking distance to the vibrant nightlife and excitement of the Antalya region. This popular Turkish accommodation offers a wide range of room types to suit families and couples. The kids can enjoy the Kids Club while the adults pamper themselves at the luxurious Harmony Spa or the private Beach Club. For a memorable and relaxing stay in Antalya, why not stay at Antalya Paradise Resort?</p>
+      <a href="book.php" class="btn">book now</a>
+   </div>
+</section>
+<!-- home offer section ends -->
+
+<button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+<!-- footer section starts  -->
+<section class="footer">
+   <div class="box-container">
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      </div>
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> (+91) 9963266865 </a>
+         <a href="#"> <i class="fas fa-phone"></i> (+91) 8466218163 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> tripsync@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Vizag, India - 530041  </a>
+      </div>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+      </div>
+   </div>
+   <div class="credit"> designed by <span>TinyCoders</span> | all rights reserved! </div>
+</section>
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+</body>
+</html>
